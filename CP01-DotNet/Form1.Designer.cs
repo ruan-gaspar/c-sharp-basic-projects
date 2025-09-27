8d82280cf261be56035b1bdc9e7b1ff981d263c5
Ruan Gaspar <123456+RuanGaspar@users.noreply.github.com>
2025-09-26 23:41:06 -0300

    Iniciando os botões

--- CP01-DotNet/Form1.Designer.cs
+++ CP01-DotNet/Form1.Designer.cs
@@ -28,11 +28,195 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        textBox1 = new TextBox();
+        label1 = new Label();
+        button1 = new Button();
+        button2 = new Button();
+        button3 = new Button();
+        button4 = new Button();
+        button5 = new Button();
+        button6 = new Button();
+        button7 = new Button();
+        button8 = new Button();
+        button9 = new Button();
+        button10 = new Button();
+        SuspendLayout();
+        // 
+        // textBox1
+        // 
+        textBox1.Location = new Point(12, 12);
+        textBox1.Name = "textBox1";
+        textBox1.Size = new Size(214, 35);
+        textBox1.TabIndex = 0;
+        textBox1.TextChanged += textBox1_TextChanged;
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Location = new Point(17, 15);
+        label1.Name = "label1";
+        label1.Size = new Size(68, 30);
+        label1.TabIndex = 1;
+        label1.Text = "label1";
+        // 
+        // button1
+        // 
+        button1.BackColor = SystemColors.ActiveCaptionText;
+        button1.Font = new Font("Lucida Console", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        button1.ForeColor = Color.Yellow;
+        button1.Location = new Point(12, 70);
+        button1.Name = "button1";
+        button1.Size = new Size(49, 60);
+        button1.TabIndex = 2;
+        button1.Text = "1";
+        button1.UseVisualStyleBackColor = false;
+        button1.Click += button1_Click;
+        // 
+        // button2
+        // 
+        button2.BackColor = SystemColors.ActiveCaptionText;
+        button2.Font = new Font("Lucida Console", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        button2.ForeColor = Color.Yellow;
+        button2.Location = new Point(67, 70);
+        button2.Name = "button2";
+        button2.Size = new Size(49, 60);
+        button2.TabIndex = 3;
+        button2.Text = "2";
+        button2.UseVisualStyleBackColor = false;
+        // 
+        // button3
+        // 
+        button3.BackColor = SystemColors.ActiveCaptionText;
+        button3.Font = new Font("Lucida Console", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        button3.ForeColor = Color.Yellow;
+        button3.Location = new Point(122, 70);
+        button3.Name = "button3";
+        button3.Size = new Size(49, 60);
+        button3.TabIndex = 4;
+        button3.Text = "3";
+        button3.UseVisualStyleBackColor = false;
+        // 
+        // button4
+        // 
+        button4.BackColor = SystemColors.ActiveCaptionText;
+        button4.Font = new Font("Lucida Console", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        button4.ForeColor = Color.Yellow;
+        button4.Location = new Point(177, 70);
+        button4.Name = "button4";
+        button4.Size = new Size(49, 60);
+        button4.TabIndex = 5;
+        button4.Text = "1";
+        button4.UseVisualStyleBackColor = false;
+        // 
+        // button5
+        // 
+        button5.BackColor = SystemColors.ActiveCaptionText;
+        button5.Font = new Font("Lucida Console", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        button5.ForeColor = Color.Yellow;
+        button5.Location = new Point(12, 149);
+        button5.Name = "button5";
+        button5.Size = new Size(49, 60);
+        button5.TabIndex = 6;
+        button5.Text = "4";
+        button5.UseVisualStyleBackColor = false;
+        // 
+        // button6
+        // 
+        button6.BackColor = SystemColors.ActiveCaptionText;
+        button6.Font = new Font("Lucida Console", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        button6.ForeColor = Color.Yellow;
+        button6.Location = new Point(67, 149);
+        button6.Name = "button6";
+        button6.Size = new Size(49, 60);
+        button6.TabIndex = 7;
+        button6.Text = "5";
+        button6.UseVisualStyleBackColor = false;
+        // 
+        // button7
+        // 
+        button7.BackColor = SystemColors.ActiveCaptionText;
+        button7.Font = new Font("Lucida Console", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        button7.ForeColor = Color.Yellow;
+        button7.Location = new Point(122, 149);
+        button7.Name = "button7";
+        button7.Size = new Size(49, 60);
+        button7.TabIndex = 8;
+        button7.Text = "6";
+        button7.UseVisualStyleBackColor = false;
+        // 
+        // button8
+        // 
+        button8.BackColor = SystemColors.ActiveCaptionText;
+        button8.Font = new Font("Lucida Console", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        button8.ForeColor = Color.Yellow;
+        button8.Location = new Point(12, 234);
+        button8.Name = "button8";
+        button8.Size = new Size(49, 60);
+        button8.TabIndex = 9;
+        button8.Text = "7";
+        button8.UseVisualStyleBackColor = false;
+        button8.Click += button8_Click;
+        // 
+        // button9
+        // 
+        button9.BackColor = SystemColors.ActiveCaptionText;
+        button9.Font = new Font("Lucida Console", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        button9.ForeColor = Color.Yellow;
+        button9.Location = new Point(122, 234);
+        button9.Name = "button9";
+        button9.Size = new Size(49, 60);
+        button9.TabIndex = 10;
+        button9.Text = "9";
+        button9.UseVisualStyleBackColor = false;
+        // 
+        // button10
+        // 
+        button10.BackColor = SystemColors.ActiveCaptionText;
+        button10.Font = new Font("Lucida Console", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        button10.ForeColor = Color.Yellow;
+        button10.Location = new Point(67, 234);
+        button10.Name = "button10";
+        button10.Size = new Size(49, 60);
+        button10.TabIndex = 11;
+        button10.Text = "8";
+        button10.UseVisualStyleBackColor = false;
+        button10.Click += button10_Click;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(12F, 30F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(312, 450);
+        Controls.Add(button10);
+        Controls.Add(button9);
+        Controls.Add(button8);
+        Controls.Add(button7);
+        Controls.Add(button6);
+        Controls.Add(button5);
+        Controls.Add(button4);
+        Controls.Add(button3);
+        Controls.Add(button2);
+        Controls.Add(button1);
+        Controls.Add(label1);
+        Controls.Add(textBox1);
+        Name = "Form1";
+        Text = "Form1";
+        ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
+
+    private TextBox textBox1;
+    private Label label1;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button button10;
 }
--- CP01-DotNet/Form1.cs
+++ CP01-DotNet/Form1.cs
@@ -6,4 +6,24 @@ public partial class Form1 : Form
     {
         InitializeComponent();
     }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button8_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button10_Click(object sender, EventArgs e)
+    {
+
+    }
 }

0ce11ee0e44f4e53222a0415883730ebe30b306b
Ruan Gaspar <ruan@archsystem.localdomain>
2025-10-23 13:11:40 -0300

    feat: relógio funcional criado

--- ClockForArch/ClockForArch/Program.cs
+++ ClockForArch/ClockForArch/Program.cs
@@ -1,3 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Gtk;
+using System.Timers;
+using Timer = System.Timers.Timer;
 
-Console.WriteLine("Hello, World!");
+class Program
+{ 
+    static Label clockLabel;
+    static System.Timers.Timer timer;
+
+    public static void Main(string[] args)
+    {
+        Application.Init();
+        
+        Window window = new Window("Relógio GTK#");
+        window.SetDefaultSize(250, 100);
+        window.DeleteEvent += delegate
+        {
+            timer.Stop();
+            Application.Quit();
+        };
+        clockLabel = new Label();
+        clockLabel.SetAlignment(0.5f, 0.5f);
+        window.Add(clockLabel);
+
+        timer = new Timer(1000);
+        timer.Elapsed += UpdateClock;
+        timer.Start();
+        
+        window.ShowAll();
+        Application.Run();
+    }
+
+    static void UpdateClock(object sender, ElapsedEventArgs e)
+    {
+        Application.Invoke(delegate
+        {
+            clockLabel.Text = DateTime.Now.ToString("HH:mm:ss");
+        });
+    }
+}
